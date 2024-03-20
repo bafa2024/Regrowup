b5c3d75dd273cf0e7668e07c947cb54bef72ba65
Fab <abdfaa12345@gmail.com>
2024-03-20 16:05:02 -0700

    update

--- pool/libs/Router.php
+++ pool/libs/Router.php
@@ -1,81 +1,31 @@
 <?php
 class Router {
-    private $routes = [];
-    private $basePath = '';
+    private $routeMap;
+    private $basePath;
 
     public function __construct($basePath = '') {
+        $this->routeMap = array();
         $this->basePath = $basePath;
     }
 
-    public function route($route, $handler) {
-        $this->routes[$route] = $handler;
+    public function addRoute($route, $target) {
+        $this->routeMap[$route] = $target;
     }
 
-    public function handleRequest($url) {
-        $url = str_replace($this->basePath, '', $url);
+    public function routeRequest() {
+        $requestUri = $_SERVER['REQUEST_URI'];
+        $requestUri = str_replace($this->basePath, '', $requestUri);
+        $requestUri = trim($requestUri, '/');
 
-        $handler = $this->findMatchingRoute($url);
-
-        if ($handler !== null) {
-            $this->callHandler($handler['handler'], $handler['params']);
-        } else {
-            $this->redirectTo('/404.html'); // Redirect to a 404 page
-        }
-    }
-
-    private function findMatchingRoute($url) {
-        foreach ($this->routes as $route => $handler) {
-            $pattern = str_replace('/', '\/', $route);
-            $pattern = "/^$pattern$/";
-
-            if (preg_match($pattern, $url)) {
-                return [
-                    'handler' => $handler,
-                    'params' => []
-                ];
+        foreach ($this->routeMap as $route => $target) {
+            if ($route === $requestUri) {
+                require $target;
+                return;
             }
         }
 
-        return null;
-    }
-
-    private function callHandler($handler, $params)
-    {
-        if (is_callable($handler)) {
-            call_user_func($handler, $params);
-        } elseif (is_string($handler)) {
-            $filePath = $_SERVER['DOCUMENT_ROOT'] . '/' . ltrim($handler, '/');
-            if (file_exists($filePath)) {
-                include $filePath;
-            } else {
-                echo "Sorry, File not found!";
-            }
-        } else {
-            echo "OOPs Invalid path!";
-        }
-    }
-
-    public function return_defaultApp($dApp)
-    {
-        switch ($dApp) {
-            case 1:
-                $defaultApp = 'edu';
-                break;
-            case 2:
-                $defaultApp = 'work';
-                break;
-            case 3:
-                $defaultApp = 'personal';
-                break;
-            default:
-                $defaultApp = null;
-                break;
-        }
-        return $defaultApp;
-    }
-
-    public function redirectTo($url) {
-        header("Location: $url");
-        exit;
+        // If no route was found, you can choose to redirect to a 404 page
+        header("HTTP/1.0 404 Not Found");
+        echo "Page not found.";
     }
 }
