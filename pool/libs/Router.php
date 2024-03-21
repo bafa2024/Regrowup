9472e436f8ed5747939c1368c4708d6365e435b5
Fab <abdfaa12345@gmail.com>
2024-03-21 13:26:41 -0700

    Update Router.php

--- pool/libs/Router.php
+++ pool/libs/Router.php
@@ -1,31 +1,87 @@
 <?php
+// Enable error reporting for debugging
+// Remove these lines in production
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 class Router {
-    private $routeMap;
-    private $basePath;
+    private $routes = [];
+    private $basePath = '';
 
     public function __construct($basePath = '') {
-        $this->routeMap = array();
         $this->basePath = $basePath;
     }
 
-    public function addRoute($route, $target) {
-        $this->routeMap[$route] = $target;
+    public function route($route, $handler) {
+        $this->routes[$route] = $handler;
     }
 
-    public function routeRequest() {
-        $requestUri = $_SERVER['REQUEST_URI'];
-        $requestUri = str_replace($this->basePath, '', $requestUri);
-        $requestUri = trim($requestUri, '/');
+    public function handleRequest($url) {
+        $url = str_replace($this->basePath, '', $url);
 
-        foreach ($this->routeMap as $route => $target) {
-            if ($route === $requestUri) {
-                require $target;
-                return;
+        $handler = $this->findMatchingRoute($url);
+
+        if ($handler !== null) {
+            $this->callHandler($handler['handler'], $handler['params']);
+        } else {
+            $this->redirectTo('/'); // Default to a 404 page
+        }
+    }
+
+    private function findMatchingRoute($url) {
+        foreach ($this->routes as $route => $handler) {
+            $pattern = str_replace('/', '\/', $route);
+            $pattern = "/^$pattern$/";
+
+            if (preg_match($pattern, $url)) {
+                return [
+                    'handler' => $handler,
+                    'params' => []
+                ];
             }
         }
 
-        // If no route was found, you can choose to redirect to a 404 page
-        header("HTTP/1.0 404 Not Found");
-        echo "Page not found.";
+        return null;
+    }
+
+    private function callHandler($handler, $params)
+    {
+        if (is_callable($handler)) {
+            call_user_func($handler, $params);
+        } elseif (is_string($handler)) {
+            $filePath = __DIR__ . '/../../' . ltrim($handler, '/');
+            if (file_exists($filePath)) {
+                include $filePath;
+            } else {
+                echo "Sorry, File not found!";
+            }
+        } else {
+            echo "OOPs Invalid path!";
+        }
+    }
+
+    public function return_defaultApp($dApp)
+    {
+        switch ($dApp) {
+            case 1:
+                $defaultApp = 'edu';
+                break;
+            case 2:
+                $defaultApp = 'work';
+                break;
+            case 3:
+                $defaultApp = 'personal';
+                break;
+            default:
+                $defaultApp = null;
+                break;
+        }
+        return $defaultApp;
+    }
+
+
+    public function redirectTo($url) {
+        header("Location: $url");
+        exit;
     }
 }
