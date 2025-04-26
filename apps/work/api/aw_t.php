b358d57a507803fca4b3f62829a7b9c4291bb49f
bafa2024 <baaysfakhri@gmail.com>
2025-04-25 22:36:40 -0700

    Update aw_t.php

--- apps/work/api/aw_t.php
+++ apps/work/api/aw_t.php
@@ -1,7 +1,13 @@
 <?php
-//session_start();
-$path=$_SERVER['DOCUMENT_ROOT'];
-include $path.'/apps/work/controllers/ExternalProjects.php';
+$path = realpath($_SERVER['DOCUMENT_ROOT'] . '/apps/work/controllers/ExternalProjects.php');
+
+if ($path && file_exists($path)) {
+    require_once $path;
+} else {
+    error_log("ExternalProjects.php not found at: $path");
+    // Optionally handle the error
+}
+
 
 
 $fr = new Bidding();
