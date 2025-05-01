3a21049e78b2ded83543cf7e6e888e84aa3c4933
bafa2024 <baaysfakhri@gmail.com>
2025-04-30 21:27:35 -0700

    Update aw_tests.php

--- apps/autowork/api/aw_tests.php
+++ apps/autowork/api/aw_tests.php
@@ -8,7 +8,7 @@ $at = new Autowork();
 //wget -O /dev/null https://regrowup.site/aw
 
 //$at->get_projects_data();
-//$at->bids_history();
+$at->bids_history();
 //$at->fetch_new_projects("php");
 
-$at->get_projects_data();
+//$at->get_projects_data();
