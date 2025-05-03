1c413f3a915441878633f6fb0487684c6aaa2066
bafa2024 <baaysfakhri@gmail.com>
2025-05-02 17:55:21 -0700

    Update Controller.php

--- apps/edu/controllers/Controller.php
+++ apps/edu/controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 //session_start();
 $path = $_SERVER['DOCUMENT_ROOT'];
-require $path . '/apps/edu/model/database.php';
+require $path . '/apps/edu/models/database.php';
 
 
 class Controller extends Database
