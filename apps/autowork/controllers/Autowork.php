7e66768381823a505ff9cd6b5040a8bc35b1279c
bafa2024 <baaysfakhri@gmail.com>
2025-05-05 13:29:06 -0700

    Update Autowork.php

--- apps/autowork/controllers/Autowork.php
+++ apps/autowork/controllers/Autowork.php
@@ -291,7 +291,7 @@ class Autowork extends Controller{
         $query= '';
 
         //$url = "https://www.freelancer.com/api/projects/0.1/projects/active/?compact=&limit=" . $limit . "&query=" . $query;
-        $url = "https://www.freelancer.com/api/projects/0.1/projects/";
+        $url = "https://www.freelancer.com/api/projects/0.1/projects/active/?compact=&limit=&query=";
         // Perform the API call
         $resp = $this->api_call($url);
         // Parse the API response to a JSON object
