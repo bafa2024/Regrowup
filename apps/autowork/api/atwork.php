a3e25d2fbcf08809f6d97e1c9d59bf402258c535
bafa2024 <baaysfakhri@gmail.com>
2025-05-06 10:04:20 -0700

    Update atwork.php

--- apps/autowork/api/atwork.php
+++ apps/autowork/api/atwork.php
@@ -7,6 +7,8 @@ $at = new Autowork();
 
 //wget -O /dev/null https://regrowup.site/aw
 
-$pids=$at->fetch_new_projects();
+//$pids=$at->fetch_new_projects();
 
-$at->get_all_projects($pids);
+$at->fetch_new_projects();
+
+//$at->get_all_projects($pids);
